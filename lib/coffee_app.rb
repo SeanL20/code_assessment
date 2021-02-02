7224abbd5e64b91afbb406560dd8427791d01be5
Sean Lim <seanl@presentplate.com>
2021-02-02 10:29:01 +1000

    Title: Add The Require For The Price List & Add The Method For The Price Loading.
    Status: Complete.

--- lib/coffee_app.rb
+++ lib/coffee_app.rb
@@ -1,7 +1,20 @@
 # Put your code here!
+require_relative 'lib/model/price_list'
 
 CoffeeApp = -> (prices_json, orders_json, payments_json){
-	puts prices_json
-	puts orders_json
-	puts payments_json
+	# order_calculations(prices_json, orders_json)
+	load_prices(prices_json)
 }
+
+def load_prices(prices_json)
+	price_list = PriceList.new
+end
+
+
+# def order_calculations(prices_json, orders_json)
+# 	order_prices = []
+
+# 	orders_json.each do |order|
+# 		puts order
+# 	end
+# end
