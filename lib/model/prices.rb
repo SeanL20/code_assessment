5a5f20b917348a3825dac8fbfe936f2ebd7be0d5
Sean Lim <seanl@presentplate.com>
2021-02-02 10:54:03 +1000

    Title: Add The Functionality To Add The Price To The Price List When Adding The New Price.
    Status: Complete.

--- lib/model/prices.rb
+++ lib/model/prices.rb
@@ -2,10 +2,12 @@ class Prices
 	attr_accessor :drink_name, :size, :price
   attr_reader :price_list
 
-	def initialize(drink_name, size, price, price_list)
+	def initialize(drink_name:, size:, price:, price_list:)
 		@drink_name = drink_name
 		@size = size
 		@price = price
 		@prices_list = price_list
+
+		@prices_list.prices << self
 	end
 end
