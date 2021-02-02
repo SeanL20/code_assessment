92ca42cdf89c6fc914a766f85df9dae6df2f1000
Sean Lim <seanl@presentplate.com>
2021-02-03 09:49:05 +1000

    Title: Create File For The Order Drinks Class.
    Status: Complete.

---
+++ lib/model/order_drinks.rb
@@ -0,0 +1,12 @@
+class OrderDrinks
+	attr_accessor :drink_name, :size, 
+  attr_reader :user_order
+	
+	def initialize(drink_name:, size:, user_order:)
+		@drink_name = drink_name
+		@size = size
+		@user_order = user_order
+
+		@user_order.drinks << self
+	end
+end
