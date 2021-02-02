bb4eed4b44970f688099d00af5fe8b15a49d76f9
Sean Lim <seanl@presentplate.com>
2021-02-02 11:02:58 +1000

    Title: Add The Functionality To Load The Prices.
    Status: Complete.

--- lib/coffee_app.rb
+++ lib/coffee_app.rb
@@ -1,5 +1,5 @@
 # Put your code here!
-require_relative 'lib/model/price_list'
+require_relative 'model/price_list'
 
 CoffeeApp = -> (prices_json, orders_json, payments_json){
 	# order_calculations(prices_json, orders_json)
@@ -8,6 +8,16 @@ CoffeeApp = -> (prices_json, orders_json, payments_json){
 
 def load_prices(prices_json)
 	price_list = PriceList.new
+
+	prices = JSON.parse(prices_json)
+
+	prices.each do |price|
+		price["prices"].each do |key, value|
+			Prices.new(drink_name: price["drink_name"], size: key, price: value, price_list: price_list)
+		end
+	end
+
+	return price_list
 end
 
 
