2ebe929c14aeec4c06a5ad8295914e0674622d7f
Sean Lim <seanl@presentplate.com>
2021-02-04 09:56:31 +1000

    Title: Add Total Price Attritube.
    Status: Complete.

--- lib/model/user_order.rb
+++ lib/model/user_order.rb
@@ -1,13 +1,14 @@
 require_relative "order_drinks"
 
 class UserOrder
-	attr_accessor :user_name, :drinks
+	attr_accessor :user_name, :drinks, :total_price
   attr_reader :users
 	
-	def initialize(user_name:, users:)
+	def initialize(user_name:, users:, total_price: nil)
 		@user_name = user_name
 		@drinks = []
 		@users = users
+		@total_price = total_price
 
 		@users.users << self
 	end
