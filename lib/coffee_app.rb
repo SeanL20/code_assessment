4f6fea6a48d9573b49ec10b1f499323b991ded64
Sean Lim <seanl@presentplate.com>
2021-02-04 10:03:28 +1000

    Title: Created Functions For Getting The Total Price For Each User Orders.
    Status: Complete.

--- lib/coffee_app.rb
+++ lib/coffee_app.rb
@@ -5,8 +5,7 @@ require_relative 'model/users'
 CoffeeApp = -> (prices_json, orders_json, payments_json){
 	price_list = load_prices(prices_json)
 	user_orders = load_orders(orders_json)
-
-
+	calculate_total_price(user_orders.users, price_list)
 }
 
 def load_prices(prices_json)
@@ -47,4 +46,22 @@ def get_user(user_name, users)
 	end
 
 	return user
+end
+
+def calculate_total_price(user_orders, price_list)
+	user_orders.each do |user_order|
+		total_price = get_total(user_order.drinks, price_list)
+		user_order.total_price = total_price
+	end
+end
+
+def get_total(drinks, price_list)
+	total_price = 0
+	drinks.each do |drink|
+		drink_price = price_list.find_by_name_size(drink_name: drink.drink_name, size: drink.size)
+
+		total_price = total_price + drink_price.price
+	end
+
+	return total_price
 end
