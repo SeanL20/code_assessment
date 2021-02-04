534bd336dd957c752166af1cf2a4925bd990cd72
Sean Lim <seanl@presentplate.com>
2021-02-05 09:10:30 +1000

    Title: Add The Balance Attribute To The User order Class.
    Status: Complete.

--- lib/model/user_order.rb
+++ lib/model/user_order.rb
@@ -1,16 +1,17 @@
 require_relative "order_drinks"
 
 class UserOrder
-	attr_accessor :user_name, :drinks, :total_price, :payment_total
+	attr_accessor :user_name, :drinks, :total_price, :payment_total, :balance
   attr_reader :users
 	
-	# initialize user class to contain user name, total price and payment_total as well as the call back for the user class and hold multiple order drinks class.
-	def initialize(user_name:, users:, total_price: nil, payment_total: nil)
+	# initialize user class to contain user name, total price, payment_total and balance as well as the call back for the user class and hold multiple order drinks class.
+	def initialize(user_name:, users:, total_price: nil, payment_total: nil, balance:nil)
 		@user_name = user_name
 		@drinks = []
 		@users = users
 		@total_price = total_price
 		@payment_total = payment_total
+		@balance = balance
 
 		@users.users << self
 	end
