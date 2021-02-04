f9404f97b51dee58a2a0c0f9944e67bfeec6a3cd
Sean Lim <seanl@presentplate.com>
2021-02-04 14:45:40 +1000

    Title: Add Payment Total Attribute To The User Order Class.
    Status: Complete.

--- lib/model/user_order.rb
+++ lib/model/user_order.rb
@@ -1,15 +1,16 @@
 require_relative "order_drinks"
 
 class UserOrder
-	attr_accessor :user_name, :drinks, :total_price
+	attr_accessor :user_name, :drinks, :total_price, :payment_total
   attr_reader :users
 	
-	# initialize user class to contain user name and total price as well as the call back for the user class and hold multiple order drinks class.
-	def initialize(user_name:, users:, total_price: nil)
+	# initialize user class to contain user name, total price and payment_total as well as the call back for the user class and hold multiple order drinks class.
+	def initialize(user_name:, users:, total_price: nil, payment_total: nil)
 		@user_name = user_name
 		@drinks = []
 		@users = users
 		@total_price = total_price
+		@payment_total = payment_total
 
 		@users.users << self
 	end
